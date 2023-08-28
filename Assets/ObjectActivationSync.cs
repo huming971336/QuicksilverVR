03ea4ecfd00bf494407749ff8722844603e63cfc
ne3lob <neslob.wr@gmail.com>
2023-08-28 03:13:33 +0200

    new mapping saal 4

--- Assets/ObjectActivationSync.cs
+++ Assets/ObjectActivationSync.cs
@@ -1,33 +1,40 @@
 using UnityEngine;
-using Photon.Pun;
+using PhotonPun = Photon.Pun;
+using PhotonRealtime = Photon.Realtime;
 
-public class ObjectActivationSync : MonoBehaviourPun
+public class ObjectActivationSync : MonoBehaviour
 {
-    public  GameObject scene;
+    public GameObject scene;
+
     private void Start()
     {
         // Set the GameObject to be active for the local player.
-        scene.SetActive(false);
     }
 
-    public void ActivateObject()
+    public void SceneToSpawn()
     {
-        
+        ActivateObject(scene);
+    }
+
+    private void ActivateObject(GameObject Prefab)
+    {
         // Call this method to enable the GameObject across the network.
-        photonView.RPC("SetActiveRPC", RpcTarget.All, true);
+        //photonView.RPC("SetActiveRPC", RpcTarget.All, true);
+        var objectToSpawn =
+            PhotonPun.PhotonNetwork.Instantiate(Prefab.name, Prefab.transform.position, Prefab.transform.rotation);
     }
 
     public void DeactivateObject()
     {
         // Call this method to disable the GameObject across the network.
-        photonView.RPC("SetActiveRPC", RpcTarget.All, false);
+        // photonView.RPC("SetActiveRPC", RpcTarget.All, false);
     }
 
     // RPC method to set the active state of the GameObject for all clients.
-    [PunRPC]
-    private void SetActiveRPC(bool isActive)
-    {
-        // Set the active state of the GameObject based on the received value.
-        scene.SetActive(isActive);
-    }
+    // [PunRPC]
+    // private void SetActiveRPC(bool isActive)
+    // {
+    //     // Set the active state of the GameObject based on the received value.
+    //     scene.SetActive(isActive);
+    // }
 }
