714109a51a72642a3fc9b68f100153615abebecf
ne3lob <neslob.wr@gmail.com>
2023-09-12 22:39:07 +0200

    add loading image animation

--- Assets/Generated/DisableObjectsOnButtonPress.cs
+++ Assets/Generated/DisableObjectsOnButtonPress.cs
@@ -8,7 +8,7 @@ public class DisableEnableObjectsOnButtonPress : MonoBehaviour
 
     [Tooltip("The button on the controller to press to disable and enable objects.")]
     public OVRInput.Button buttonToPressB = OVRInput.Button.Two;
-    
+
 
     [Tooltip("The objects to disable and enable when the button is pressed.")]
     public GameObject[] objectsToDisableEnable;
@@ -16,27 +16,47 @@ public class DisableEnableObjectsOnButtonPress : MonoBehaviour
     public GameObject performerEyes;
 
     private bool objectsDisabled = false;
+    public string targetTag = "Anchor";
 
     private void Update()
     {
         if (OVRInput.Get(buttonToPressA))
         {
             if (!objectsDisabled)
-            {
+                
+            { 
+                GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(targetTag);
                 foreach (GameObject obj in objectsToDisableEnable)
                 {
                     obj.SetActive(false);
+                    foreach (GameObject objTag in objectsWithTag)
+                    {
+                        // Deactivate all children of the object
+                        foreach (Transform child in objTag.transform)
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+                    }
                 }
 
                 objectsDisabled = true;
             }
         }
-        
+
         if (OVRInput.Get(buttonToPressA) && OVRInput.Get(buttonToPressB))
         {
+            GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(targetTag);
             foreach (GameObject obj in objectsToDisableEnable)
             {
                 obj.SetActive(true);
+                foreach (GameObject objTag in objectsWithTag)
+                {
+                    // Deactivate all children of the object
+                    foreach (Transform child in objTag.transform)
+                    {
+                        child.gameObject.SetActive(true);
+                    }
+                }
             }
 
             objectsDisabled = false;
