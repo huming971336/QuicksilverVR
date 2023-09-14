77b819b4074b921b5b6be251b227ab633d9cb2f5
bapek <67985782+bapek@users.noreply.github.com>
2023-09-14 16:11:04 +0300

    speed change Fader, Radiation Image, Amazing Manager, Winter Organization

--- Assets/Generated/Fader.cs
+++ Assets/Generated/Fader.cs
@@ -8,29 +8,39 @@ public class Fader : MonoBehaviourPun
     public float fadeSpeed = 1f;
 
     private SpriteRenderer spriteRenderer;
+    
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (PhotonNetwork.LocalPlayer.NickName[0] + "" != "T")
+        {
+            GameObject objectToActivate = GameObject.FindGameObjectWithTag("BlackFader");
+            spriteRenderer = objectToActivate.GetComponent<SpriteRenderer>();
+
+        }
     }
 
     [PunRPC]
-    private void ChangeAlpha(float targetAlpha)
+    private void ChangeAlpha(float targetAlpha, float changeSpeed)
     {
-        StartCoroutine(ChangeAlphaCoroutine(targetAlpha));
+        //if getting object at start not working we can move it to here
+        if (PhotonNetwork.LocalPlayer.NickName[0] + "" != "T")
+        {
+            StartCoroutine(ChangeAlphaCoroutine(targetAlpha, changeSpeed));
+        }
     }
 
-    public void FadeOut()
+    public void FadeOut(float changeSpeed)
     {
-        photonView.RPC("ChangeAlpha", RpcTarget.All, 0f);
+        photonView.RPC("ChangeAlpha", RpcTarget.All, 0f, changeSpeed);
     }
 
-    public void FadeIn()
+    public void FadeIn(float changeSpeed)
     {
-        photonView.RPC("ChangeAlpha", RpcTarget.All, 1f);
+        photonView.RPC("ChangeAlpha", RpcTarget.All, 1f, changeSpeed);
     }
 
-    private IEnumerator ChangeAlphaCoroutine(float targetAlpha)
+    private IEnumerator ChangeAlphaCoroutine(float targetAlpha, float changeSpeed)
     {
         Color currentColor = spriteRenderer.color;
         float currentAlpha = currentColor.a;
