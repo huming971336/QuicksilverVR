2305a3e433b9011ceb772739a499f24b36e1cc1d
ne3lob <neslob.wr@gmail.com>
2023-09-16 12:12:17 +0200

    add prefabs to the scene

--- Assets/ObjectActivationSync.cs
+++ Assets/ObjectActivationSync.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using PhotonPun = Photon.Pun;
 using PhotonRealtime = Photon.Realtime;
+using UnityEngine;
+using System.Collections;
+using Photon.Pun;
 
 public class ObjectActivationSync : MonoBehaviour
 {
     [Tooltip("Tag of the Photon Network object to delete.")] [SerializeField]
     private string objectTag = "scenes";
+    public GameObject[] objectsToActivate;
+    public GameObject[] objectsToActivatethorns;
+    public float activationDelay = 3f;
+
+    private int currentIndex = 0;
+    private int currentIndexThorn=0;
+
 
     private void Start()
     {
@@ -24,6 +34,58 @@ public class ObjectActivationSync : MonoBehaviour
         //photonView.RPC("SetActiveRPC", RpcTarget.All, true);
         var objectToSpawn =
             PhotonPun.PhotonNetwork.Instantiate(Prefab.name, Prefab.transform.position, Prefab.transform.rotation);
+    }
+    public void ObjectInventinghope()
+    {
+        StartCoroutine(ActivateObjectsWithDelay());
+    }
+    
+    private IEnumerator ActivateObjectsWithDelay()
+    {
+        
+        Debug.Log(currentIndex);
+        while (currentIndex < objectsToActivate.Length)
+        {
+            GameObject currentObject = objectsToActivate[currentIndex];
+            
+            // Check if the object is not null and has a PhotonView.
+            if (currentObject != null && currentObject.GetComponent<PhotonView>() != null)
+            {
+                // Activate the object locally (on all clients).
+                PhotonNetwork.Instantiate(currentObject.name, currentObject.transform.position, currentObject.transform.rotation);
+            }
+
+            currentIndex++;
+
+            // Wait for the next activation delay.
+            yield return new WaitForSeconds(activationDelay);
+        }
+    }
+    public void ObjectThorns()
+    {
+        StartCoroutine(ActivateThornsObjectsWithDelay());
+    }
+
+    
+    private IEnumerator ActivateThornsObjectsWithDelay()
+    {
+        
+        while (currentIndexThorn < objectsToActivatethorns.Length)
+        {
+            GameObject currentObject = objectsToActivatethorns[currentIndex];
+            
+            // Check if the object is not null and has a PhotonView.
+            if (currentObject != null && currentObject.GetComponent<PhotonView>() != null)
+            {
+                // Activate the object locally (on all clients).
+                PhotonNetwork.Instantiate(currentObject.name, currentObject.transform.position, currentObject.transform.rotation);
+            }
+
+            currentIndex++;
+
+            // Wait for the next activation delay.
+            yield return new WaitForSeconds(activationDelay);
+        }
     }
 
 
